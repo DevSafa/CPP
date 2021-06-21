e7ec308c9353edcb497419f4b7476cebc018b5b0
SafaBarka <sbarka@e0r2p3.1337.ma>
2021-06-21 13:03:10 +0100

    compile and execute a c++ program  + writes to the screen

---
+++ hello.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+//to compile : gcc hello.cpp -lstdc++
+
+//to compile a c++ project g++ hello.cpp 
+
+//g++ hello.cpp -o hello
+
+int main()
+{
+    std::cout << "hello wolrd!\n";
+
+    std::cout << "-----------\n";
+
+    std::cout << "       1\n";
+    std::cout << "     2 3\n";
+    std::cout << "   4 5 6\n";
+    std::cout << "7 8 9 10\n";
+
+    std::cout << "-----------\n";
+
+    std::cout << "Dear Self,\n";
+    std::cout << "Build a VR game.\n";
+    std::cout << "1/1/2020, New York\n";
+}
+
