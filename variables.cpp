292bbb3ab56153d684d823ad3e34572f421c293b
SafaBarka <sbarka@e0r2p3.1337.ma>
2021-06-21 13:21:20 +0100

    declare a variable , assign to it , change it's value and print it

---
+++ variables.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int main()
+{
+    //declare a variable
+    int score;
+    
+    //declare and initialize a variable
+    int year = 2019;
+    //assign to the score variable
+    score = 0;
+
+    //change score here
+    score = 1234 * 99;
+
+    //output the variable.
+    std::cout << score << "\n";
+    std::cout << score;
+    return 0;
+}
