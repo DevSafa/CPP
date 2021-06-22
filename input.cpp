861d5b717e1d9e2ed79fb573ddb5f24f49975d13
SafaBarka <sbarka@e0r2p8.1337.ma>
2021-06-22 15:13:52 +0100

    get the user input

---
+++ input.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main()
+{
+    int tip = 0;
+
+    std::cout << "Enter tip amount: ";
+    //what the user enter gets saved in the int variable named tip
+    std::cin >> tip;
+
+     std::cout << "You paid " << tip <<" dollars.\n";
+
+}
