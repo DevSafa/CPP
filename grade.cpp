21375d6f1737b939f3fe831a9a2c6f94345df2c2
SafaBarka <sbarka@e0r2p8.1337.ma>
2021-06-22 15:39:54 +0100

    if - else if - else statements

---
+++ grade.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int main()
+{
+    int grade = 90;
+    if(grade > 60)
+        std::cout << "Pass\n";
+    else
+        std::cout << "Fail\n";
+
+
+    double ph = 4.6;
+    if (ph > 7)
+        std::cout << "Basic\n";
+    else if (ph < 7)
+        std::cout << "Acidic\n";
+    else
+        std::cout << "Neutral\n";
+
+}
