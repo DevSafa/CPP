5ad2943a4d60d93c859a80f98ae0f9ae12a4e966
SafaBarka <sbarka@e0r3p11.1337.ma>
2021-06-28 20:32:39 +0100

    Module04-ex04 : filestreams input and output

---
+++ CPP-MODULES/Module01/ex04/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+int main(int argc , char *argv[])
+{
+    std::string  extension = ".replace"; 
+    std::string out = argv[1] + extension;
+    std::ofstream my_output_file(out);
+   
+    std::ifstream my_input_file(argv[1]);
+
+    std::string my_string;
+
+    std::string  s1 = argv[2];
+    std::string  s2 = argv[3];
+    int index;
+
+    while(getline(my_input_file,my_string)){
+        index = my_string.find(s1);
+        while(index != std::string::npos)
+        {
+            my_string  = my_string.substr(0,index) +s2+
+            my_string.substr(index +s1.length(),my_string.length() - index +s1.length() + 1);
+            index = my_string.find(s1);
+        }
+        my_output_file << my_string << std::endl;
+    }
+    // while(getline(my_input_file,my_string)){
+    //     index = my_string.find(s1);
+    //     if(index != std::string::npos)
+    //         my_string  = my_string.substr(0,index) +s2+ my_string.substr(index +s1.length(),my_string.length() - index +s1.length() + 1);
+    //     my_output_file << my_string << std::endl;
+    // }
+}
