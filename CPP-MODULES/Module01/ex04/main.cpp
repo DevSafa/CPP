eca68f7e21528fae41398fe1c775898fd9dfaf55
sbarka-safa <sbarka@s1r2p3.1337.ma>
2021-07-03 21:15:06 +0100

    fix problem in Module01-ex04

--- CPP-MODULES/Module01/ex04/main.cpp
+++ CPP-MODULES/Module01/ex04/main.cpp
@@ -14,14 +14,15 @@ int main(int argc , char *argv[])
     std::string  s1 = argv[2];
     std::string  s2 = argv[3];
     int index;
-
+    std::string tmp;
     while(getline(my_input_file,my_string)){
         index = my_string.find(s1);
         while(index != std::string::npos)
         {
+            tmp = my_string.substr(0,index) +s2;
             my_string  = my_string.substr(0,index) +s2+
             my_string.substr(index +s1.length(),my_string.length() - index +s1.length() + 1);
-            index = my_string.find(s1);
+            index = my_string.find(s1,index + tmp.length());
         }
         my_output_file << my_string << std::endl;
     }
