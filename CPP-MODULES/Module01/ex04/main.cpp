b2602643709dab90c7a3ff5b68ad4f584f6ee536
sbarka-safa <sbarka@e1r12p4.1337.ma>
2021-08-29 13:42:42 +0100

    Module06-ex04

--- CPP-MODULES/Module01/ex04/main.cpp
+++ CPP-MODULES/Module01/ex04/main.cpp
@@ -1,35 +1,75 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+
+void replace(std::ifstream *my_input_file, std::ofstream *my_output_file)
+{
+    
+}
 int main(int argc , char *argv[])
 {
+    /* test number of arguments */
+    if(argc != 4)
+    {
+        std::cout << "Invalid number of arguments!!!" << std::endl;
+        return 0;
+    }
+    std::ifstream my_input_file(argv[1]);
+
+    /* test if the file failed to open because of bad permisison , doesn't exist */
+    if (my_input_file.fail())
+    {
+        std::cout << "input file Error!!!" << std::endl;
+        return 0;
+    }
+
+    /* test if the file is empty  */
+    if(my_input_file.peek() == std::ifstream::traits_type::eof())
+    {
+        std::cout << "file is empty!!!" << std::endl;
+        return 0;
+    }
+    //check if s1 and s2 are empty
+    std::string  s1 = argv[2];
+    std::string  s2 = argv[3];
+    if(s1.empty() || s2.empty())
+    {
+        std::cout << "use of empty string!!!" << std::endl;
+        return 0;
+    }
     std::string  extension = ".replace"; 
     std::string out = argv[1] + extension;
     std::ofstream my_output_file(out);
+
+    /*failed to open output file */
+    if(my_output_file.fail())
+    {
+        std::cout << "output file error!!!" << std::endl;
+        return 0;
+    }
    
-    std::ifstream my_input_file(argv[1]);
-
     std::string my_string;
-
-    std::string  s1 = argv[2];
-    std::string  s2 = argv[3];
-    int index;
-    std::string tmp;
+    unsigned long index;
+    
     while(getline(my_input_file,my_string)){
         index = my_string.find(s1);
         while(index != std::string::npos)
         {
-            tmp = my_string.substr(0,index) + s2;
             my_string  = my_string.substr(0,index) +s2+
             my_string.substr(index +s1.length(),my_string.length() - index +s1.length() + 1);
             index = my_string.find(s1,index + s2.length());
         }
         my_output_file << my_string << std::endl;
     }
-    // while(getline(my_input_file,my_string)){
-    //     index = my_string.find(s1);
-    //     if(index != std::string::npos)
-    //         my_string  = my_string.substr(0,index) +s2+ my_string.substr(index +s1.length(),my_string.length() - index +s1.length() + 1);
-    //     my_output_file << my_string << std::endl;
-    // }
+    
+    my_output_file.close();
 }
+
+/*
+    The function peek() is used with input streams, and returns the next character in the stream 
+    or EOF if the end of file is read. peek() does not remove the character from the stream.
+
+    npos is a static member constant value
+    This constant is defined with a value of -1
+
+*/
