2c16a4360a574ecd8241447ced1e306f27609ca9
sbarka-safa <sbarka@e1r12p4.1337.ma>
2021-08-29 14:57:01 +0100

    Module06-ex06

--- CPP-MODULES/Module01/ex06/main.cpp
+++ CPP-MODULES/Module01/ex06/main.cpp
@@ -3,36 +3,40 @@
 
 int main(int argc , char *argv[])
 {
-    int level;
+    if(argc != 2)
+    {
+        std::cout << "Invalid number of arguments!!!" << std::endl;
+        return 0;
+    }
+    int level = -1;
     std::string levels[4];
     levels[0]="DEBUG";
     levels[1]="INFO";
     levels[2] ="WARNING";
     levels[3]="ERROR";
 
-    int i = 0;
-    while(i < 4)
+    
+    for(int i = 0 ; i < 4 ; i++)
     {
-        if(levels[i] == argv[1])
+         if(levels[i] == argv[1])
         {
             level = i;
             break;
         }
-        i++;
     }
-
+   Karen karen;
     switch(level)
     {
         case 0:
-            std::cout << "[ Debug ]\n" << "I love to get extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger.\nI just love it!\n\n" ;
+            karen.complain("debug");
         case 1:
-            std::cout << "[ INFO ]\n" << "I cannot believe adding extra bacon cost more money.\nYou don’t put enough! If you did I would not have to ask for it!\n\n" ;
+            karen.complain("info");
         case 2:
-            std::cout << "[ WARNING ]\n" << "I think I deserve to have some extra bacon for free.\nI’ve been coming here for years and you just started working here last month.\n\n";
+            karen.complain("warning");
         case 3:
-            std::cout << "[ ERROR ]\n" << "I think I deserve to have some extra bacon for free.\nI’ve been coming here for years and you just started working here last month.\n\n";
+            karen.complain("error");
             break;
         default :
-            std ::cout << "[ Probably complaining about insignificant problems ]";
+            std ::cout << "[ Probably complaining about insignificant problems ]\n";
     }
 }
