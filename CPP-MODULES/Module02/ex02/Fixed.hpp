d397c624da9df2134b2612473ad0e14629a3b1f9
sbarka-safa <sbarka@e1r12p5.1337.ma>
2021-08-30 19:08:52 +0100

    start Module06-ex02

---
+++ CPP-MODULES/Module02/ex02/Fixed.hpp
@@ -0,0 +1,31 @@
+#ifndef FIXED_HPP
+# define FIXED_HPP
+#include <cmath>
+#include <iostream>
+class Fixed {
+    private :
+        int _fixedPointValue;
+        static const int _bitFractio;
+    public :
+        Fixed( void );
+        ~Fixed( void );
+        Fixed(Fixed const & src);
+        Fixed const & operator = (Fixed  const & src);
+        int getRawBits( void ) const; 
+        void setRawBits( int const raw);
+        Fixed( int const & x);
+        Fixed( float const & x);
+        float toFloat( void )const;
+        int toInt( void ) const;
+        bool operator > (Fixed const & src);
+        bool operator < (Fixed const & src);
+        bool operator >= (Fixed const & src);
+        bool operator <= (Fixed const & src);
+        bool operator == (Fixed const & src);
+        bool operator != (Fixed const & src);
+
+};
+
+std::ostream & operator << (std::ostream & o ,Fixed const & src);
+
+#endif
